1882fe4cbac895a1eda52e6dff05040c42fde21d
Akeen11 <aartkeen@ut.utm.edu>
2018-07-24 21:57:06 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 1
+
+for (let y = 1; y < cookies.length; y++) {
+    const currentCookie = cookies[y]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
+
+const conjunction = function (firstWord, secondWord) {
+    conjoinedWord = `${firstWord} ${secondWord}`
+}
+
+conjunction("Master", "Card")
+console.log(conjoinedWord)
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968", 
+            "end": "1973"
+        }
+    }
+
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    ModSquad.members.forEach(member => {
+        HTMLRepresentation += `<div>${member}</div>`
+        console.log(HTMLRepresentation)
+    })
+    document.querySelector(".show-info").innerHTML = HTMLRepresentation
+}
